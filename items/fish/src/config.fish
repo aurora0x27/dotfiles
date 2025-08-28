1241ccb99636682232b82f0f4e5a2dd19be86400
aurora0x27 <qingfengzl0831@163.com>
2025-08-29 01:41:33 +0800

    [Fish] Add starship prompt theme

--- items/fish/src/config.fish
+++ items/fish/src/config.fish
@@ -1,3 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set -x STARSHIP_CONFIG "$HOME/.config/starship/tokyo_night.toml"
+    starship init fish | source
 end
