76515f09283b1757d0153e29c63dab8a468aff03
aurora0x27 <qingfengzl0831@163.com>
2025-10-24 02:39:23 +0800

    [Waybar] Format code

--- items/waybar/src/scripts/power.sh
+++ items/waybar/src/scripts/power.sh
@@ -8,7 +8,7 @@ else
     color_class="normal"
 fi
 if [ "$online" -eq 1 ]; then 
-    echo {\"text\": \"[󱐋 ${cap}% *󰚥]\", \"class\": \"$color_class\"}; 
+    echo {\"text\": \"[󱐋 ${cap}% 󰚥]\", \"class\": \"$color_class\"}; 
 else
     echo {\"text\": \"[󱐋 ${cap}%]\", \"class\": \"$color_class\"};
 fi
