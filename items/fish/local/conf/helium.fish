5d819a65acbadc70331b107e557c353f0472c473
aurora0x27 <qingfengzl0831@163.com>
2026-05-10 21:49:17 +0800

    [Fish] Fix on macos

--- items/fish/local/conf/helium.fish
+++ items/fish/local/conf/helium.fish
@@ -7,4 +7,16 @@ fish_add_path /opt/homebrew/bin \
 
 set -gx LLVM_HOME "/opt/homebrew/opt/llvm@20"
 set -gx RUSTFLAGS "$RUSTFLAGS -A dead_code"
-set -gx MallocNanoZone=0
+set -gx MallocNanoZone 0
+
+set -gx CPLUS_INCLUDE_PATH "$HOME/.local/include"
+set -gx HTTP_PROXY 'http://127.0.0.1:2080'
+set -gx HTTPS_PROXY 'http://127.0.0.1:2080'
+set -gx VCPKG_ROOT "$HOME/.local/opt/vcpkg"
+
+fish_add_path /opt/homebrew/opt/openjdk@17/bin
+fish_add_path /opt/homebrew/opt/texinfo/bin
+
+alias vi-git '~/.local/opt/nvim/bin/nvim'
+
+alias z=zathura
