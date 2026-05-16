1f58d23a380f452f3988f977ceff75be48daecff
aurora0x27 <qingfengzl0831@163.com>
2026-05-16 15:49:56 +0800

    [Starship] Tweak kali prompt

--- items/starship/src/scripts/path.sh
+++ items/starship/src/scripts/path.sh
@@ -13,14 +13,18 @@ if [[ "$dir" == ~* ]]; then
     prefix="~"
     remaining_parts=("${parts[@]:1}")
 else
+    if (( ${#parts[@]} <= 2 )); then
+        echo "$dir"
+        exit 0
+    fi
     prefix="/${parts[1]}"
-    remaining_parts=("${parts[@]}")
+    remaining_parts=("${parts[@]:2}")
 fi
 
-if (( ${#remaining_parts[@]} > 5 )); then
-    last4=("${remaining_parts[@]: -4}")
-    joined_last4=$(printf "/%s" "${last4[@]}")
-    echo "$prefix/…${joined_last4}"
+if (( ${#remaining_parts[@]} > 3 )); then
+    last3=("${remaining_parts[@]: -3}")
+    printf -v joined_last3 "/%s" "${last3[@]}"
+    echo "${prefix}/…${joined_last3}"
 else
     echo "$dir"
 fi
