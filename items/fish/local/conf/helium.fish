615089e22dcdcc1eacaa5cb79ca9e7d292d919da
aurora0x27 <qingfengzl0831@163.com>
2026-06-24 22:58:09 +0800

    [Fish] Bump llvm version on `helium`

--- items/fish/local/conf/helium.fish
+++ items/fish/local/conf/helium.fish
@@ -1,11 +1,11 @@
 fish_add_path /opt/homebrew/bin \
               /opt/homebrew/opt/rustup/bin \
-              /opt/homebrew/opt/llvm@20/bin \
+              /opt/homebrew/opt/llvm/bin \
               "$HOME/.local/bin" \
               "$HOME/.cargo/bin" \
               "/Library/Input Methods/Fcitx5.app/Contents/bin"
 
-set -gx LLVM_HOME "/opt/homebrew/opt/llvm@20"
+set -gx LLVM_HOME "/opt/homebrew/opt/llvm"
 set -gx RUSTFLAGS "$RUSTFLAGS -A dead_code"
 set -gx MallocNanoZone 0
 
