dfb863fa525c7789e9cb3d17d882960bf1c0d297
aurora0x27 <qingfengzl0831@163.com>
2026-07-04 23:31:07 +0800

    [Niri] Tweak clipbord history
    - Move the window to the middle of screen
    
    - Mute the warning message when nothing selected
    
    - Tweak color, layout and prompt

--- items/niri/src/scripts/fzf-clip.sh
+++ items/niri/src/scripts/fzf-clip.sh
@@ -2,23 +2,22 @@
 
 # Set fzf theme
 export FZF_DEFAULT_OPTS=" \
---color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 \
+--color=bg+:#313244,bg:-1,spinner:#F5E0DC,hl:#F38BA8 \
 --color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
 --color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
 --color=selected-bg:#45475A \
 --color=border:#6C7086,label:#CDD6F4"
 
 selected=$(cliphist list | fzf \
-  --prompt=" > " \
+  --prompt="> " \
   --border \
   --info hidden \
   --preview='$HOME/.config/niri/scripts/preview.sh {}' \
   --preview-window=right:60%:wrap \
+  --layout=reverse \
   --bind 'ctrl-u:preview-up,ctrl-d:preview-down' \
-  --height=90% --border)
+  --height=100% --border)
 
 if [[ -n $selected ]]; then
     cliphist decode "$selected" | wl-copy
-else
-    notify-send "[Warn] fzf-clip selected nothing"
 fi
